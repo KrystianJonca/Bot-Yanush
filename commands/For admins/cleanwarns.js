9ca0aa321802bf05643f1a526f3c7523cc3cea88
KrystianJonca <33162450+KrystianJonca@users.noreply.github.com>
2018-05-12 15:48:28 +0200

    Corrections in cleanwarns command

--- commands/For admins/cleanwarns.js
+++ commands/For admins/cleanwarns.js
@@ -9,11 +9,11 @@ module.exports.run = async (bot,message,args,prefix) => {
     let reason = args.join(" ").slice(22);
     
     if (!cleanUser) return React.sendReact(false,message,"You did not specify a user mention or ID!","reply");
+    if (cleanUser.id === message.author.id) return React.sendReact(false,message,"You cannot clean warns yourself!","reply");
     if (cleanUser.id === bot.user.id) return React.sendReact(false,message,"I dont't have warns","send");
     if (!reason) return React.sendReact(false,message,"You must give a reason!","reply");
     if (!message.member.hasPermission("KICK_MEMBERS")) return React.sendReact(false,message,"You don't have require permission!","reply");
     if (cleanUser.hasPermission("KICK_MEMBERS")) return React.sendReact(false,message,"That person can't be warned!","reply");
-    if (!warns[cleanUser.id]) return React.sendReact(false,message,"That user doesn't have warns!","reply");
 
     warns[cleanUser.id] = {
         warnings: 0
@@ -42,7 +42,7 @@ module.exports.run = async (bot,message,args,prefix) => {
     return;
 }
 module.exports.config = {
-    name: ["cleanwarn"],
+    name: ["cleanwarns"],
     args:"@user <reason>",
     description: "Clean user's warns"
 }
